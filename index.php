81072796e9bbfdee9a04e2de9ab9873a492d6f2b
Imesh Gunaratne <imesh@apache.org>
2015-05-22 04:19:27 +0530

    Updating index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <head>
   <title>Apache Straots PHP Demo</title>
   <link href="style.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 </head>
-
 <body>
-  <div id="main">
+  <div class="center" id="main">
   <img src="images/stratos-logo.png" style="width: 150px" />
   <h1>Apache Stratos PHP Demo</h1>
-  <?php echo "<h2>Running Container IP - ".$_SERVER['SERVER_ADDR']."</h2>"; ?>
+  <?php echo "<h2>Container IP: ".$_SERVER['SERVER_ADDR']."</h2>"; ?>
 
   <?php include("footer.php"); ?>
   </div>
